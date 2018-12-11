a541a433637642e2967035af809b5b7c03d3eb01
Pramendra Gupta <pramendra@gmail.com>
2018-12-11 19:43:19 +0900

    configurated nightwatch

---
+++ e2e/index.js
@@ -0,0 +1,15 @@
+module.exports = Object.assign(
+  {
+    before: (browser, done) => {
+      // runs before all of the tests run
+      done(); // tell nightwatch we're done after we have done all of our bootstrapping
+    },
+    after: (browser, done) => {
+      // runs after all of the tests run
+      browser.end(); // kill the browser
+      done(); // tell nightwatch we're done
+    },
+  },
+  // tests
+  require('./routes.test')(),
+);
---
+++ e2e/routes.test.js
@@ -0,0 +1,21 @@
+const baseURL = 'http://localhost:3000';
+const routes = [
+  { name: 'home', path: '/' },
+  { name: 'category', path: 'category' },
+];
+ function getEnvName() {
+  const idx = process.argv.findIndex(arg => arg === '-e' || arg === '--env');
+  return idx > -1 ? process.argv[idx + 1] : undefined;
+}
+ module.exports = () => ({
+  'Routes - test mobile and desktop page loads properly': browser => {
+    const env = getEnvName();
+    routes.map(({ name, path }) => {
+      return browser
+        .url(`${baseURL}${path}`)
+        .waitForElementVisible('body')
+        .saveScreenshot(`./reports/${env}-${name}-page.png`);
+    });
+     browser.end();
+  },
+});
