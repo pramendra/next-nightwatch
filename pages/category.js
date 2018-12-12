63ac176a8d06c361ae8233bb7c783ac43b8f2f6c
Pramendra Gupta <pramendra@gmail.com>
2018-12-12 11:28:59 +0900

    fixed typo

--- pages/category.js
+++ pages/category.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default () => (
   <h1>
     <Link href="/">
-      <a id="home-link">Link to Home page</a>
+      <a id="home-link">Link to Home Page</a>
     </Link>
   </h1>
 );
